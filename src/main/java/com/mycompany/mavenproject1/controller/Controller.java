308494c4249c502f52a72b7ff9b6cba69709e11e
Robert Rook <robertrook@zonnet.nl>
2016-11-29 23:26:27 +0100

    Haakje vergeten

--- src/main/java/com/mycompany/mavenproject1/controller/Controller.java
+++ src/main/java/com/mycompany/mavenproject1/controller/Controller.java
@@ -71,7 +71,7 @@ public class Controller {
             
             default: verwerkInput_menu();
                      break;
-                     
+        }         
                      
         
     }
